e812227bebeebd357698ae300076a3620b515734
Luke Daley <ld@ldaley.com>
2013-12-03 14:53:57 -0500

    Create method

--- src/ratpack/Ratpack.groovy
+++ src/ratpack/Ratpack.groovy
@@ -4,6 +4,7 @@ import ratpack.example.books.Book
 import ratpack.groovy.sql.SqlModule
 import ratpack.h2.H2Module
 
+import static ratpack.form.Forms.form
 import static ratpack.groovy.Groovy.groovyTemplate
 import static ratpack.groovy.Groovy.ratpack
 
@@ -16,13 +17,26 @@ ratpack {
     handlers { Sql sql ->
 
         sql.executeInsert("create table if not exists books (id int primary key auto_increment, title varchar(255), content varchar(255))")
-        sql.executeUpdate("merge into books (id, title, content) key (id) values (0, 'Book 1', 'Stuff')")
 
         get {
             def books = sql.rows("select id, title, content from books order by id").collect { GroovyRowResult result ->
                 new Book(result.id, result.title, result.content)
             }
             render groovyTemplate("listing.html", title: "Books", books: books, msg: request.queryParams.msg ?: "")
+        }
+
+        handler("create") {
+            byMethod {
+                get {
+                    render groovyTemplate("create.html", title: "Create Book")
+                }
+                post {
+                    def form = parse form()
+                    def ids = sql.executeInsert("insert into books (title, content) values ($form.title, $form.content)")
+                    def id = ids[0][0]
+                    redirect "/?msg=Book+$id+created"
+                }
+            }
         }
 
         assets "public"
