ef4f77fba184316c358494430285cfb5d8cfb3a8
Luke Daley <ld@ldaley.com>
2013-12-03 16:55:53 -0500

    JSON api

--- src/ratpack/Ratpack.groovy
+++ src/ratpack/Ratpack.groovy
@@ -1,15 +1,19 @@
 import ratpack.example.books.BookService
 import ratpack.groovy.sql.SqlModule
 import ratpack.h2.H2Module
+import ratpack.jackson.JacksonModule
 
 import static ratpack.form.Forms.form
 import static ratpack.groovy.Groovy.groovyTemplate
 import static ratpack.groovy.Groovy.ratpack
+import static ratpack.jackson.Jackson.json
+import static ratpack.jackson.Jackson.jsonNode
 
 ratpack {
     modules {
         register new H2Module()
         register new SqlModule()
+        register new JacksonModule()
     }
 
     handlers { BookService bookService ->
@@ -60,6 +64,44 @@ ratpack {
             } else {
                 bookService.delete(id)
                 redirect "/?msg=Book+$id+deleted"
+            }
+        }
+
+        prefix("api") {
+            get("books") {
+                render json(bookService.list())
+            }
+            handler("book/:id?") {
+                def id = pathTokens.asLong("id")
+                def book = id ? bookService.find(id) : null
+
+                if (!request.method.post && (id == null || book == null)) {
+                    return clientError(404)
+                }
+
+                byMethod {
+                    post {
+                        if (id != null) {
+                            clientError 404
+                        } else {
+                            def input = parse jsonNode()
+                            def newId = bookService.insert(input.get("title").asText(), input.get("content").asText())
+                            render json(bookService.find(newId))
+                        }
+                    }
+                    get {
+                        render json(book)
+                    }
+                    put {
+                        def input = parse jsonNode()
+                        bookService.update(id, input.get("title").asText(), input.get("content").asText())
+                        render json(bookService.find(id))
+                    }
+                    delete {
+                        bookService.delete(id)
+                        response.send()
+                    }
+                }
             }
         }
 
