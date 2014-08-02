cf64bbe3afe0fee1e23eb2e5392bc4fdd5021c2b
danhyun <hyun.danny+github@gmail.com>
2014-08-02 19:35:21 -0400

    updating to 0.9.7 and updating httpclient usage

--- src/main/groovy/ratpack/example/books/IsbnDbCommands.groovy
+++ src/main/groovy/ratpack/example/books/IsbnDbCommands.groovy
@@ -3,8 +3,11 @@ package ratpack.example.books
 import com.google.inject.Inject
 import com.netflix.hystrix.HystrixCommandGroupKey
 import com.netflix.hystrix.HystrixObservableCommand
+import ratpack.func.Action
+import ratpack.http.HttpUrlSpec
 import ratpack.http.client.HttpClients
 import ratpack.http.client.ReceivedResponse
+import ratpack.http.client.RequestSpec
 import ratpack.launch.LaunchConfig
 
 import static ratpack.rx.RxRatpack.observe
@@ -22,8 +25,18 @@ class IsbnDbCommands {
         return new HystrixObservableCommand<ReceivedResponse>(HystrixCommandGroupKey.Factory.asKey("http-isbndb")) {
             @Override
             protected rx.Observable<ReceivedResponse> run() {
-                return observe(HttpClients.httpClient(launchConfig).
-                        get("http://isbndb.com/api/v2/json/${launchConfig.getOther('isbndb.apikey', '')}/book/$isbn"))
+                return observe(HttpClients.httpClient(launchConfig)
+                    .get(new Action<RequestSpec>() {
+                        @Override
+                        void execute(RequestSpec request) throws Exception {
+                            request.url(new Action<HttpUrlSpec>() {
+                                @Override
+                                void execute(HttpUrlSpec httpUrlSpec) throws Exception {
+                                    httpUrlSpec.set("http://isbndb.com/api/v2/json/${launchConfig.getOther('isbndb.apikey', '')}/book/$isbn".toURI())
+                                }
+                            })
+                        }
+                    }))
             }
 
             @Override
