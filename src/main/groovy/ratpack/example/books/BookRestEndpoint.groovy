1500436e56f6a9d159ab162bb9f1edea0f20bd29
rhart <russellxhart@googlemail.com>
2014-09-28 18:06:16 +0100

    Update after GroovyChainAction deletion

--- src/main/groovy/ratpack/example/books/BookRestEndpoint.groovy
+++ src/main/groovy/ratpack/example/books/BookRestEndpoint.groovy
@@ -1,13 +1,15 @@
 package ratpack.example.books
 
-import ratpack.groovy.handling.GroovyChainAction
+import ratpack.func.Action
+import ratpack.groovy.Groovy
+import ratpack.handling.Chain
 
 import javax.inject.Inject
 
 import static ratpack.jackson.Jackson.json
 import static ratpack.jackson.Jackson.jsonNode
 
-class BookRestEndpoint extends GroovyChainAction {
+class BookRestEndpoint implements Action<Chain> {
 
     private final BookService bookService
 
@@ -16,59 +18,60 @@ class BookRestEndpoint extends GroovyChainAction {
         this.bookService = bookService
     }
 
-
     @Override
-    protected void execute() throws Exception {
-        handler(":isbn") {
-            def isbn = pathTokens["isbn"]
+    void execute(Chain chain) throws Exception {
+        Groovy.chain(chain) {
+            handler(":isbn") {
+                def isbn = pathTokens["isbn"]
 
-            byMethod {
-                get {
-                    bookService.find(isbn).single().subscribe { Book book ->
-                        if (book == null) {
-                            clientError 404
-                        } else {
-                            render book
+                byMethod {
+                    get {
+                        bookService.find(isbn).single().subscribe { Book book ->
+                            if (book == null) {
+                                clientError 404
+                            } else {
+                                render book
+                            }
+                        }
+                    }
+                    put {
+                        def input = parse jsonNode()
+                        bookService.update(
+                                isbn,
+                                input.get("quantity").asLong(),
+                                input.get("price").asDouble()
+                        ) flatMap {
+                            bookService.find(isbn).single()
+                        } subscribe { Book book ->
+                            render json(book)
+                        }
+                    }
+                    delete {
+                        bookService.delete(isbn).subscribe {
+                            response.send()
                         }
                     }
                 }
-                put {
-                    def input = parse jsonNode()
-                    bookService.update(
-                        isbn,
-                        input.get("quantity").asLong(),
-                        input.get("price").asDouble()
-                    ) flatMap {
-                        bookService.find(isbn).single()
-                    } subscribe { Book book ->
-                        render json(book)
-                    }
-                }
-                delete {
-                    bookService.delete(isbn).subscribe {
-                        response.send()
-                    }
-                }
             }
-        }
 
-        handler {
-            byMethod {
-                get {
-                    bookService.all().toList().subscribe { List<Book> books ->
-                        render json(books)
+            handler {
+                byMethod {
+                    get {
+                        bookService.all().toList().subscribe { List<Book> books ->
+                            render json(books)
+                        }
                     }
-                }
-                post {
-                    def input = parse jsonNode()
-                    bookService.insert(
-                        input.get("isbn").asText(),
-                        input.get("quantity").asLong(),
-                        input.get("price").asDouble()
-                    ).single().flatMap {
-                        bookService.find(it).single()
-                    } subscribe { Book createdBook ->
-                        render json(createdBook)
+                    post {
+                        def input = parse jsonNode()
+                        bookService.insert(
+                                input.get("isbn").asText(),
+                                input.get("quantity").asLong(),
+                                input.get("price").asDouble()
+                        ).single().flatMap {
+                            bookService.find(it).single()
+                        } subscribe { Book createdBook ->
+                            render json(createdBook)
+                        }
                     }
                 }
             }
