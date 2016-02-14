d00edb822198249799da4671394905ac14993bf4
rhart <rus@gradle.com>
2016-02-14 18:29:47 +0000

    Merge https://github.com/ratpack/example-books/pull/22

--- src/test/groovy/GebConfig.groovy
+++ src/test/groovy/GebConfig.groovy
@@ -1,1 +1,13 @@
+import org.openqa.selenium.firefox.FirefoxDriver
+import org.openqa.selenium.remote.DesiredCapabilities
+import org.openqa.selenium.Proxy
+
+driver = {
+    DesiredCapabilities capabilities = DesiredCapabilities.firefox();
+    Proxy proxy = new Proxy()
+    proxy.setProxyType(Proxy.ProxyType.DIRECT)
+    capabilities.setCapability("proxy", proxy)
+    def driver = new FirefoxDriver(capabilities);
+}
+
 reportsDir = "build/geb-reports"
